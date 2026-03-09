b2a1d0d8929993243003165ca60f2f76a1c91b19
christopheribok <ibokchris@gmail.com>
2026-03-09 02:05:10 +0100

    Footer Correction fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,58 +9,59 @@ import TestimonialsSection from "@/components/landing/TestimonialsSection";
 import AboutSection from "@/components/landing/AboutSection";
 import ContactCTA from "@/components/landing/ContactCTA";
 
-// Update these lines in your Home.js file:
 const homepageStructuredData = [
   {
     "@context": "https://schema.org",
     "@type": "ProfessionalService",
-    name: "NeedMo Consult",
-    // ... other fields
-    telephone: "+1-(850) 267-2327", // <--- CHANGE THIS
-    email: "hello@needmoconsult.com",
-    address: {
-      "@type": "35080",
-      streetAddress: "1005 Woodlands Cv", // <--- CHANGE THIS
-      addressLocality: "Helena", // <--- CHANGE THIS
-      addressRegion: "Alabama",      // <--- CHANGE THIS
-      addressCountry: "US",
+    "name": "NeedMo Consult",
+    "url": "https://needmoconsult.com/",
+    "logo": "https://needmoconsult.com/logo.png",
+    "image": "https://needmoconsult.com/og-image.jpg",
+    "telephone": "+1-530-626-4474", 
+    "email": "hello@needmoconsult.com",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "882 Maria Way",
+      "addressLocality": "Placerville",
+      "addressRegion": "CA",
+      "postalCode": "95667",
+      "addressCountry": "US"
     },
-    // ...
-    priceRange: "$400-$4500",
-    areaServed: "Worldwide",
-    serviceType: [
+    "priceRange": "$400-$4500",
+    "areaServed": "Worldwide",
+    "serviceType": [
       "Social Media Management",
       "Content Creation",
       "Paid Advertising",
-      "Social Media Strategy",
+      "Social Media Strategy"
     ],
-    sameAs: [
+    "sameAs": [
       "https://www.facebook.com/needmoconsult",
       "https://www.instagram.com/needmoconsult",
       "https://www.linkedin.com/company/needmoconsult",
-      "https://twitter.com/needmoconsult",
-    ],
+      "https://twitter.com/needmoconsult"
+    ]
   },
   {
     "@context": "https://schema.org",
     "@type": "Organization",
-    name: "NeedMo Consult",
-    aggregateRating: {
+    "name": "NeedMo Consult",
+    "aggregateRating": {
       "@type": "AggregateRating",
-      ratingValue: "5",
-      reviewCount: "50",
+      "ratingValue": "5",
+      "reviewCount": "50"
     },
-    review: [
+    // FIXED: Correctly nested inside the Organization object
+    "review": [
       {
         "@type": "Review",
-        author: { "@type": "Person", name: "Sarah Chen" },
-        datePublished: "2025-12-15",
-        reviewBody:
-          "NEEDMO transformed our social presence. In 3 months, we saw a 250% increase in engagement.",
-        reviewRating: { "@type": "Rating", ratingValue: "5" },
-      },
-    ],
-  },
+        "author": { "@type": "Person", "name": "Sarah Chen" },
+        "datePublished": "2025-12-15",
+        "reviewBody": "NEEDMO transformed our social presence. In 3 months, we saw a 250% increase in engagement.",
+        "reviewRating": { "@type": "Rating", "ratingValue": "5" }
+      }
+    ]
+  }
 ];
 
 export default function Home() {
